1cbb3eb168538c021bd5fb3790ddad87241ac83b
EL MAHDI ATMANI <mahdiaatmani@gmail.com>
2024-05-12 17:32:42 +0100

    establishment modal finish

--- academic-planner-app/src/app/models/Establishment.ts
+++ academic-planner-app/src/app/models/Establishment.ts
@@ -1,4 +1,10 @@
+import { City } from "./City";
 
 export class Establishment {
-    
+    id: number;
+    code: string;
+    name: string;
+    description: string;
+    thumbnail:string; 
+    city: City;
 }
