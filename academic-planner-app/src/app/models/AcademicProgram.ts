65b2fd8a1576b072a2ee0a13cb4b255fddb2a55f
EL MAHDI ATMANI <mahdiaatmani@gmail.com>
2024-05-12 18:49:14 +0100

    models ceartion & intial course modal

---
+++ academic-planner-app/src/app/models/AcademicProgram.ts
@@ -0,0 +1,10 @@
+import { Degree } from "./Degree";
+import { Department } from "./Department";
+
+export class AcademicProgram {
+    id:Number;
+    code:String;   
+    name:String ;
+    department:Department ;
+    degree :Degree;
+}
---
+++ academic-planner-app/src/app/models/Department.ts
@@ -0,0 +1,9 @@
+import { Establishment } from "./Establishment";
+
+export class Department {
+    id: number;
+    code: string;
+    name: string;
+    rank: number;
+    establishment: Establishment;
+}
